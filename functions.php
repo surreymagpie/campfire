62f9c7d35c31e9a7b744ed23def12e2ba76d3bc9
Rob Curtis <surreymagpie@yahoo.co.uk>
2020-02-23 19:34:07 +0000

    Include lighter font weight

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ Engueue Assets
 */
 function campfire_scripts()
 {
-  wp_enqueue_style( 'google', 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,900' );
+  wp_enqueue_style( 'google', 'https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,700,900' );
   wp_enqueue_style( 'campfire', get_stylesheet_directory_uri() . '/assets/css/campfire.css' );
 
   wp_enqueue_script( 'campfire', get_stylesheet_directory_uri() . '/assets/js/campfire.js', array(), false, true );
