0af8f5857e5c3654d8983ab823cf928b70d93084
Rob Curtis <surreymagpie@yahoo.co.uk>
2020-03-01 22:31:44 +0000

    Style navigation for small screens

--- assets/js/campfire.js
+++ assets/js/campfire.js
@@ -11,3 +11,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	html.classList.remove('no-js');
 	html.classList.add('js')
 });
+
+/*
+===============================
+Toggle navigation menu
+===============================
+*/
+
+const menu = document.getElementById('menu-main');
+const toggle = document.getElementById('menu-toggle');
+
+toggle.addEventListener('click', function() {
+	menu.classList.toggle('is-open');
+	setTimeout(() => {
+		toggle.classList.toggle('inverted');
+	}, 400);
+});
