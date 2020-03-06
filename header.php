6526792675d9b401d0a374d2b076e1ee9e1aa3bd
Rob Curtis <surreymagpie@yahoo.co.uk>
2020-03-06 21:52:05 +0000

    Add viewport `meta` tag to fix mobile display
    This commit fixes issue #1

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ) ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title( ' | ', true, 'right' ) . bloginfo( 'name' ); ?></title>
 	<?php wp_head(); ?>
 </head>
