3f1f3bb9d0d02ad74f911702e508300b07e5c4ba
Rob Curtis <surreymagpie@yahoo.co.uk>
2020-03-08 14:12:06 +0000

    Set up BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-const   { src, dest, series }       = require ('gulp');
-const   sass                        = require ('gulp-sass');
-        sass.compiler               = require ('node-sass');
+const   { src, dest, series, watch }    = require ('gulp');
+const   sass                            = require ('gulp-sass');
+        sass.compiler                   = require ('node-sass');
+const   browserSync                     = require ('browser-sync').create();
+const   settings                        = require ('./settings')
 
 // Compile Sass files into CSS
 function styles() {
@@ -9,7 +11,28 @@ function styles() {
         outputStyle: 'expanded',
         sourceMap: false
         }).on('error', sass.logError))
-    .pipe(dest('./assets/css'));
+    .pipe(dest('./assets/css'))
+    .pipe(browserSync.stream());
 }
 
-exports.default = styles;
+// Static server
+function browsersync(done) {
+    browserSync.init({
+        proxy: settings.urlToPreview
+    });
+    done();
+};
+
+function reload(done) {
+    browserSync.reload();
+	done();
+}
+
+function watch_files() {
+    watch('src/sass/**/*.s(a|c)ss', styles);
+    watch('(*.php|inc/*.php)', reload);
+}
+
+exports.styles  = styles;
+exports.serve   = browsersync;
+exports.default = series(styles, browsersync, watch_files);
