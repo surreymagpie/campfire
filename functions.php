6af3ce0fdc64dec2087a56fcc4842f40b89ba0c2
Rob Curtis <surreymagpie@yahoo.co.uk>
2020-03-15 12:35:20 +0000

    Prevent OSM plugin CSS loading

--- functions.php
+++ functions.php
@@ -13,11 +13,14 @@ function campfire_scripts()
 	} else {
 		$version = $theme->Version;
 	}
-	
-  wp_enqueue_style( 'google', 'https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,700,900' );
-  wp_enqueue_style( 'campfire', get_stylesheet_directory_uri() . '/assets/css/campfire.css', array(), $version );
 
-  wp_enqueue_script( 'campfire', get_stylesheet_directory_uri() . '/assets/js/campfire.js', array(), $version, true );
+	//   Unload OSM Plugin CSS
+	wp_dequeue_style( 'OSM: Patrol Points' );
+	
+	wp_enqueue_style( 'google', 'https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,700,900' );
+	wp_enqueue_style( 'campfire', get_stylesheet_directory_uri() . '/assets/css/campfire.css', array(), $version );
+
+	wp_enqueue_script( 'campfire', get_stylesheet_directory_uri() . '/assets/js/campfire.js', array(), $version, true );
 }
 add_action( 'wp_enqueue_scripts', 'campfire_scripts' );
 
